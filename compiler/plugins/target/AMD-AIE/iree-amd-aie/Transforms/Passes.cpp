4f6452de4873d761e4f5c80af85141a2bf8fd598
Yu-Zhewen <zhewenyu@amd.com>
2024-12-05 13:40:14 +0000

    init commit

--- compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Passes.cpp
+++ compiler/plugins/target/AMD-AIE/iree-amd-aie/Transforms/Passes.cpp
@@ -667,6 +667,7 @@ void addAMDAIEObjectFifoLoweringPasses(
   passManager.addPass(createAMDAIEAssignPacketIdsPass());
 
   passManager.addPass(createAMDAIENpuDmaToHalfDmaCpyNdPass());
+  passManager.addPass(createAMDAIESimplifyDmaWaitsPass());
   passManager.addPass(createAMDAIEControlCodeLoweringPass());
   passManager.addPass(createAMDAIEControlCodeToTransactionPass());
 
